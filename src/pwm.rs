4c81a7ed7d28a673e7403323447fffb7b29b1c21
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-05 10:07:20 +0200

    Simplify PwmExt

--- src/pwm.rs
+++ src/pwm.rs
@@ -50,43 +50,43 @@ impl<P: gpio::GPIOPin, C: timer::TimerChannel, AF: gpio::AltFnNum> hal::PwmPin
 }
 
 /// PwmExtension trait
-pub trait PwmExt {
-    /// type of pin
-    type P: gpio::GPIOPin;
-    /// type of channel
-    type C: timer::TimerChannel;
-    /// type for AF
-    type AF: gpio::AltFnNum;
+pub trait PwmExt<SP: gpio::OutputSpeed,
+                 C: timer::TimerChannel,
+                 AF: gpio::AltFnNum>: gpio::GPIOPin
+{
+    /// type
+    type OutputPin: gpio::GPIOPin;
     /// binding
-    type Output;
+    type Output: hal::PwmPin;
     /// Configures pin and channel to create pwm binding
-    fn new(pin: Self::P, channel: Self::C) -> Self::Output;
+    fn to_pwm(self, channel: C, sp: SP) -> Self::Output;
 }
 
 macro_rules! pwm {
     (
-        $CRFN:ident,($PINMOD:ident, $PIN:ident),($TIM:ident, $CHN:ident, $CHPE:expr),($AF:ident, $PP:ident, $SPEED:ident)
+        $CRFN:ident,($PINMOD:ident, $PIN:ident),($TIM:ident, $CHN:ident, $CHPE:expr),($AF:ident, $PP:ident)
     ) => {
-        impl<PT: gpio::PullType, PM: gpio::PinMode, CM: timer::ChMode> PwmExt
-            for PwmBinding<gpio::$PINMOD::$PIN<PT, PM>,
-                           timer::$TIM::Channel<timer::$CHN, CM>,
-                           gpio::$AF>
+        impl<SP: gpio::OutputSpeed,
+              PT: gpio::PullType,
+              PM: gpio::PinMode,
+              CM: timer::ChMode>
+            PwmExt<SP, timer::$TIM::Channel<timer::$CHN, CM>, gpio::$AF>
+            for gpio::$PINMOD::$PIN<PT, PM>
         {
-            type P = gpio::$PINMOD::$PIN<PT, PM>;
-            type C = timer::$TIM::Channel<timer::$CHN, CM>;
-            type AF = gpio::$AF;
-            type Output =
-                PwmBinding<gpio::$PINMOD::$PIN<PT,
-                                               gpio::AltFn<gpio::$AF,
-                                                           gpio::$PP,
-                                                           gpio::$SPEED>>,
-                           timer::$TIM::Channel<timer::$CHN, timer::Pwm1>,
-                           gpio::$AF>;
-            fn new(pin: Self::P, channel: Self::C) -> Self::Output {
-                let pin = pin.alternating(gpio::$AF)
-                             .output_speed(gpio::$SPEED)
-                             .output_type(gpio::$PP)
-                             .alt_fn(gpio::$AF);
+            type OutputPin =
+                gpio::$PINMOD::$PIN<PT, gpio::AltFn<gpio::$AF, gpio::$PP, SP>>;
+            type Output = PwmBinding<Self::OutputPin,
+                                     timer::$TIM::Channel<timer::$CHN,
+                                                          timer::Pwm1>,
+                                     gpio::$AF>;
+            fn to_pwm(self,
+                      channel: timer::$TIM::Channel<timer::$CHN, CM>,
+                      sp: SP)
+                      -> Self::Output {
+                let pin = self.alternating(gpio::$AF)
+                              .output_speed(sp)
+                              .output_type(gpio::$PP)
+                              .alt_fn(gpio::$AF);
                 let mut channel = channel.mode(timer::Pwm1);
                 channel.preload($CHPE);
                 PwmBinding { pin,
@@ -100,22 +100,6 @@ macro_rules! pwm {
                        timer::$TIM::Channel<timer::$CHN, CM>,
                        gpio::$AF>
         {
-            /// Binds PIN to Timer's channel and configures them to act as pwm;
-            /// you can also use ::new with type annotations.
-            pub fn $CRFN(
-                pin: gpio::$PINMOD::$PIN<PT, PM>,
-                channel: timer::$TIM::Channel<timer::$CHN, CM>)
-                -> PwmBinding<gpio::$PINMOD::$PIN<PT,
-                                                  gpio::AltFn<gpio::$AF,
-                                                              gpio::$PP,
-                                                              gpio::$SPEED>>,
-                              timer::$TIM::Channel<timer::$CHN, timer::Pwm1>,
-                              gpio::$AF> {
-                PwmBinding::<gpio::$PINMOD::$PIN<_, _>,
-                           timer::$TIM::Channel<timer::$CHN, _>,
-                           gpio::$AF>::new(pin, channel)
-            }
-
             /// Modify channel's preload
             pub fn channel_preload(&mut self, enabled: bool) {
                 self.channel.preload(enabled)
@@ -124,80 +108,74 @@ macro_rules! pwm {
     };
 }
 
-// XXX: don't force speed?
 // XXX: don't force Pwm1? allow Pwm2 as well?
 
 pwm!(bind_pa0_tim2_ch1,
      (gpioa, PA0),
      (tim2, CH1, true),
-     (AF1, PushPull, MediumSpeed));
+     (AF1, PushPull));
 
 pwm!(bind_pa1_tim2_ch2,
      (gpioa, PA1),
      (tim2, CH2, true),
-     (AF1, PushPull, MediumSpeed));
+     (AF1, PushPull));
 
 pwm!(bind_pa2_tim2_ch3,
      (gpioa, PA2),
      (tim2, CH3, true),
-     (AF1, PushPull, MediumSpeed));
+     (AF1, PushPull));
 
 pwm!(bind_pa3_tim2_ch4,
      (gpioa, PA3),
      (tim2, CH4, true),
-     (AF1, PushPull, MediumSpeed));
-
-pwm!(bind_pa0_tim2_ch1_af2,
-     (gpioa, PA0),
-     (tim2, CH1, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF1, PushPull));
 
 pwm!(bind_pc6_tim3_ch1,
      (gpioc, PC6),
      (tim3, CH1, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pc7_tim3_ch1,
      (gpioc, PC7),
      (tim3, CH2, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pc8_tim3_ch1,
      (gpioc, PC8),
      (tim3, CH3, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pc9_tim3_ch1,
      (gpioc, PC9),
      (tim3, CH4, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb0_tim3_ch3,
      (gpiob, PB0),
      (tim3, CH3, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb1_tim3_ch4,
      (gpiob, PB1),
      (tim3, CH4, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb6_tim4_ch1,
      (gpiob, PB6),
      (tim4, CH1, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb7_tim4_ch2,
      (gpiob, PB7),
      (tim4, CH2, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb8_tim4_ch3,
      (gpiob, PB8),
      (tim4, CH3, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
 
 pwm!(bind_pb9_tim4_ch4,
      (gpiob, PB9),
      (tim4, CH4, true),
-     (AF2, PushPull, MediumSpeed));
+     (AF2, PushPull));
