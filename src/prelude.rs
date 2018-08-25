34a81b387cf9585a4dc86b07a7431c15d6842ecc
Roma Sokolov <sokolov.r.v@gmail.com>
2018-08-26 01:16:08 +0200

    Export SpiExt in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -6,4 +6,5 @@ pub use hal::prelude::*;
 pub use pwm::PwmExt as _stm32f103xx_hal_pwm_PwmExt;
 pub use rcc::RccExt as _stm32f30x_hal_rcc_RccExt;
 pub use serial::SerialExt as _stm32f30x_hal_serial_SerialExt;
+pub use spi::SpiExt as _stm32f30x_hal_spi_SpiExt;
 pub use time::U32Ext as _stm32f30x_hal_time_U32Ext;
