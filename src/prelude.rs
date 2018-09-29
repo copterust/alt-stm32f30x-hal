0d89c9e377a3c53340f145a2714335027e535289
Roma Sokolov <sokolov.r.v@gmail.com>
2018-09-29 21:02:59 +0200

    Expose I2cExt in prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,6 +3,7 @@
 pub use flash::FlashExt as _stm32f30x_hal_flash_FlashExt;
 pub use gpio::GpioExt as _stm32f30x_hal_gpio_GpioExt;
 pub use hal::prelude::*;
+pub use i2c::I2cExt as _stm32f30x_hal_i2ci_I2cExt;
 pub use pwm::PwmExt as _stm32f103xx_hal_pwm_PwmExt;
 pub use rcc::RccExt as _stm32f30x_hal_rcc_RccExt;
 pub use serial::SerialExt as _stm32f30x_hal_serial_SerialExt;
